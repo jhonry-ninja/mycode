b7e6f79001dc228e44db68cba2821dc923e0586a
jhonry-ninja <jhonry-ninja@users.noreply.github.com>
2021-04-05 20:43:28 +0000

    About complex lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# create a list called animals
+animals = ['Fox', 'Fly', 'Ant', 'Bee', 'Cod', 'Cat', 'Dog', 'Yak', 'Cow', 'Hen', 'Koi', 'Hog', 'Jay', 'Kit'
+]
+
+# display list animals
+print(*animals)
