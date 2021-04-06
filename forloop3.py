662814d9feff2a6e67e9f2825d82c3be9ee815e8
jhonry-ninja <jhonry-ninja@users.noreply.github.com>
2021-04-06 21:41:13 +0000

    Looping techniques

---
+++ forloop3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# create a list of dictionaries
+farms = [{"name": "NE Farm", "agriculture": ["sheep", "cows", "pigs", "chickens", "llamas", "cats"]},
+         {"name": "W Farm", "agriculture": ["pigs", "chickens", "llamas"]},
+         {"name": "SE Farm", "agriculture": ["chickens", "carrots", "celery"]}]
+
+for x in farms:
+    print("\nThe " + farms.keys(), "produces" + farms.values())
+
