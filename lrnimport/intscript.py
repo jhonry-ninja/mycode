8429ef75d2df9dee7a98c8bafc1023560e9b8e7b
jhonry-ninja <jhonry-ninja@users.noreply.github.com>
2021-04-07 22:49:01 +0000

    matplotlib graphing

---
+++ lrnimport/intscript.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""JCOpinaldo | Practicing from & import Statements"""
+
+from subprocess import call
+
+def main():
+    call(["ip", "link", "show", "up"])
+    print("This program will check your interfaces.")
+    
+    interface = input("Enter an interface, like, ens3: ")
+    call(["ip", "addr", "show", "dev", interface])
+    call(["ip", "route", "show", "dev", interface])
+main();
